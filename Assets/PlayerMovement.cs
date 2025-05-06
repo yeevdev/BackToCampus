bed5183bdce361b8a1fc771f8a75c1a7d22c0243
Heonwoo <162540185+heonwoos@users.noreply.github.com>
2025-05-06 22:24:49 +0900

    feat: 플레이어 이동 구현
    - 360도 이동 구현
    - 플레이어 이동 범위 구현
    - 플레이어의 y좌표가 upBound 이상이면 플레이어가 정지하고 맵이 내려오도록 구현

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speedCoef, upBound, downBound, horizontalBound;
+    // 속도와 플레이어가 움직일 수 있는 경계를 직접 설정해주세요
+    [SerializeField] private Joystick joystick;  // Inspector에서 조이스틱을 직접 설정해주세요
+    [SerializeField] private GameObject gameMap; // Inspector에서 맵을 직접 설정해주세요 
+    private Transform _transform, _gameMapTransform;
+
+    private void Start()
+    {
+        // caching
+        _transform = transform;
+        _gameMapTransform = gameMap.transform;
+    }
+
+    private void Update()
+    {
+        Vector3 velocity = joystick.getInputVector() * speedCoef; // 조이스틱 조작에 따른 플레이어 속도
+
+        _transform.position += velocity;
+
+        _transform.position = new Vector3( // 플레이어 이동 범위 제한
+            Mathf.Clamp(_transform.position.x, -horizontalBound, horizontalBound),
+            Mathf.Clamp(_transform.position.y, downBound, upBound)
+        );
+
+        if (_transform.position.y >= upBound && velocity.y > 0) // 플레이어가 upBound보다 위에 있고 위를 향하고 있다면
+            _gameMapTransform.position -= Vector3.up * velocity.y; // 맵을 아래로 이동
+    }
+}
