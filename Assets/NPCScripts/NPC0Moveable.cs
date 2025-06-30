9d02e43b4f7db48edf0f328e5ec7b2ed9cfb0f73
Heonwoo <162540185+heonwoos@users.noreply.github.com>
2025-07-01 02:12:00 +0900

    feat: NPC 이동 구현
    - 필요 없는 IMove() 제거
    - NPC0Moveable: direction을 동서남북 중 하나 선택하여 이동
    - NPC1Moveable: 아래로만 이동
    - 두 NPC 모두 부드럽게 이동 구현(코드에서 interpolation으로 지칭)

--- Assets/NPCScripts/NPC0Moveable.cs
+++ Assets/NPCScripts/NPC0Moveable.cs
@@ -1,5 +1,70 @@
+using Unity.Collections;
 using UnityEngine;
-public class NPC0Moveable : NPC0, IMove
+public class NPC0Moveable : NPC0
 {
+    [SerializeField] private float stepSize; // 한 스텝의 길이
+    [SerializeField] private float maxTime; // 움직이는데 걸리는 시간
+    [SerializeField] private float maxInterpolationTime; // 부드럽게 움직이는 시간 
+    private bool isMoving, isInterpolating;
+    // isMoving: 이동하는 전체 과정 중 true
+    // isInterpolating: 한 칸 이동하는 동안 true
+    private float time, interpolationTime;
+    private Vector3[] direction = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.down,
+        Vector3.left,
+    };
+    private Vector3 displacement;
 
+
+    private void OnEnable()
+    {   // 풀에서 가져오거나 생성되었을 때 초기화
+        InitMovement();
+    }
+
+    public void InitMovement()
+    {
+        isMoving = false;
+        time = maxTime;
+        InitInterpolation();
+    }
+
+    public void InitInterpolation()
+    {
+        isInterpolating = false;
+        interpolationTime = 0;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            if (isInterpolating)
+            {
+                if (interpolationTime < maxInterpolationTime)
+                {
+                    interpolationTime += Time.deltaTime;
+                    transform.position += Time.deltaTime / maxInterpolationTime * displacement;
+                }
+                else InitInterpolation();
+            }
+            else
+            {
+                time -= Time.deltaTime;
+                if (time <= 0)
+                {
+                    Vector3 chosenDirection = direction[Random.Range(0, 4)]
+                    chosenDirection 
+                    displacement = stepSize * c;
+                    time = maxTime;
+                    isInterpolating = true;
+                }
+            }
+        }
+    }
+    void OnBecameVisible()
+    {
+        isMoving = true;
+    }
 }
--- Assets/NPCScripts/NPC1Moveable.cs
+++ Assets/NPCScripts/NPC1Moveable.cs
@@ -1,5 +1,70 @@
 using UnityEngine;
-public class NPC1Moveable : NPC1, IMove
+public class NPC1Moveable : NPC1
 {
+    [SerializeField] private float stepSize; // 한 스텝의 길이
+    [SerializeField] private float maxStep; // 최대 스텝 수
+    [SerializeField] private float maxTime; // 움직이는데 걸리는 시간
+    [SerializeField] private int maxInterpolationTime; // 서브스텝 개수; 부드럽게 움직이기 위해 한 스텝의 길이를 몇 개로 쪼갤 것인지
+    private bool isMoving, isInterpolating;
+    // isMoving: 이동하는 전체 과정 중 true
+    // isInterpolating: 한 칸 이동하는 동안 true
+    private float time, interpolationTime;
+    private int stepCount = 0; // 현재 스텝 수
+    private Vector3 displacement;
 
+
+      private void OnEnable()
+    {   // 풀에서 가져오거나 생성되었을 때 초기화
+        stepCount = 0;
+        InitMovement();
+    }
+
+    public void InitMovement()
+    {
+        isMoving = false;
+        time = maxTime;
+        InitInterpolation();
+    }
+
+    public void InitInterpolation()
+    {
+        isInterpolating = false;
+        interpolationTime = 0;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            if (isInterpolating)
+            {
+                if (interpolationTime < maxInterpolationTime)
+                {
+                    interpolationTime += Time.deltaTime;
+                    transform.position += Time.deltaTime / maxInterpolationTime * displacement;
+                }
+                else InitInterpolation();
+            }
+            else
+            {
+                time -= Time.deltaTime;
+                if (time <= 0)
+                {
+                    if (stepCount < maxStep)
+                    {
+                        displacement = stepSize * Vector3.down;
+                        time = maxTime;
+                        isInterpolating = true;
+                        stepCount++;
+                    }
+                    else InitMovement();
+                }
+            }
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        isMoving = true;
+    }
 }
