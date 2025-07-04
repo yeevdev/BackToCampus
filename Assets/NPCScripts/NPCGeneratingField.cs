da07a372bc2a113b23652a4cbfcf0b3d067f2b53
Heonwoo <162540185+heonwoos@users.noreply.github.com>
2025-07-05 02:44:18 +0900

    feat: NPC가 연속적인 구간이 아닌 정해진 점 중에서 생성되도록 변경
    - NPC는 5*3개의 큰 구역 중 4개가 정해진 뒤, 각 구역을 2*2로 나누어 그 중앙에 생성됨
    
    - GetRandomCoords()의 패러미터에 int subrows, int subcolumns (2*2)추가.

--- Assets/NPCScripts/NPCGeneratingField.cs
+++ Assets/NPCScripts/NPCGeneratingField.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 public class NPCGeneratingField : MonoBehaviour
 {
     [SerializeField] private ObjectPoolingManager poolingManager;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private List<Sprite> skins;
+
 
     private void FixedUpdate()
     {
@@ -13,18 +15,40 @@ public class NPCGeneratingField : MonoBehaviour
 
     private void SpawnNPCs(int numberOfNPCs)
     {
+        Vector2[] coords = GetRandomCoords(5, 3, 2, 2, numberOfNPCs);
+
         // 4종류의 NPC Prefab 중 랜덤으로 선택
         List<int> types = new();
         for (int i = 0; i < numberOfNPCs; i++)
         {
+             
             types.Add(Random.Range(0, 4));
         }
 
-        Vector2[] coords = GetRandomCoords(5, 3, numberOfNPCs);
-
         for (int i = 0; i < numberOfNPCs; i++)
         {
-            int type = types[i];
+            
+            int type = Random.Range(0, 4);
+
+            // if (/*same column exists*/) {
+            //     if (/*type is 0*/) {
+            //         /*the other cell in the same column should be type 0, 1, or 2 */
+            //         // type 3 is forbidden
+            //     }
+            //     else if (/*type is 1*/) {
+            //         /*the other cell in the same column should be type 0, 1, 2 or 3*/
+            //         // nothing is forbidden
+            //     }
+            //     else if (/*type is 2*/) {
+            //         /*the other cell in the same column should be type 0, 1, or 2 */
+            //         // type 3 is forbidden
+            //     }
+            //     else if (/*type is 3*/) {
+            //         /*the other cell in the same column should only be type 1 or 3*/
+            //         // type 0 or 2 is forbidden
+            //     }
+            // }
+
             NPC newNPC = poolingManager.pools[type].Get().GetComponent<NPC>();
             if (type % 2 == 0) // NPC0과 NPC0Moveable
             {
@@ -37,7 +61,7 @@ public class NPCGeneratingField : MonoBehaviour
         }
     }
     
-    private Vector2[] GetRandomCoords(int rows, int columns, int numberOfNPCs)
+    private Vector2[] GetRandomCoords(int rows, int columns, int subrows, int subcolumns, int numberOfNPCs)
     {
         float cellWidth = Screen.width / columns;
         float cellHeight = Screen.height / rows;
@@ -49,20 +73,30 @@ public class NPCGeneratingField : MonoBehaviour
 
         for (int i = 0; i < numberOfNPCs; i++)
         {
-            int chosenIndex = Random.Range(0, rows * columns - i);
-            int cellNum = cells[chosenIndex];
+            // 선택되지 않은 큰 구역 중 한 곳을 정함
+            int chosenIndex = Random.Range(0, cells.Count);
+            int chosenCell = cells[chosenIndex];
 
-            Vector3 position = new(
-                cellWidth * (cellNum % columns) + Random.Range(0f, cellWidth),
-                Screen.height + cellHeight * (cellNum / columns) + Random.Range(0f, cellHeight),
-                0f
-                );
+            // 선택된 구역을 더 잘게 나누는 subcell을 정함
+            int chosenSubcell = Random.Range(0, subrows * subcolumns - 1);
+            // 한 subcell의 너비와 높이
+            float subcellWidth = cellWidth / subcolumns;
+            float subcellHeight = cellHeight / subrows;
+
+            // NPC가 생성될 좌표
+            Vector2 position = new(
+                cellWidth * (chosenCell % columns) + subcellWidth * (chosenSubcell % subcolumns + .5f),
+                Screen.height + cellHeight * (chosenCell / columns) + subcellHeight * (chosenSubcell / subcolumns + .5f)
+            );
 
             positions[i] = mainCamera.ScreenToWorldPoint(position);
 
+            // 선택된 큰 구역의 숫자를 제거
             cells.RemoveAt(chosenIndex);
         }
 
+        var sorted = from coord in positions orderby coord.x select coord;
+        positions = sorted.ToArray();
         return positions;
     }
 }
