728b596bba0bea6d28e681d7d419dae4fec9fb39
Heonwoo <162540185+heonwoos@users.noreply.github.com>
2025-07-07 23:38:23 +0900

    feat: 랜덤추적형 NPC가 플레이어와 가까이 있으면 쫓아옴
    isChasing 변수가 true가 되면 쫓아옴
    NPC0Moveable에 Detector를 놓아서 Detector의 CircleCollider2D에 닿으면 OnTriggerEnter()로 인해 추적 시작, 나오면 추적 끝.

--- Assets/NPCScripts/NPC0Moveable.cs
+++ Assets/NPCScripts/NPC0Moveable.cs
@@ -4,19 +4,22 @@ public class NPC0Moveable : NPC0
     [SerializeField] private float stepSize; // 한 스텝의 길이
     [SerializeField] private float maxTime; // 움직이는데 걸리는 시간
     [SerializeField] private float maxInterpolationTime; // 부드럽게 움직이는 시간 
-    [SerializeField] private bool isMoving, isInterpolating;
+    [SerializeField] private bool isMoving, isInterpolating, isChasing;
     // isMoving: 이동하는 전체 과정 중 true
     // isInterpolating: 한 칸 이동하는 동안 true
     [SerializeField] private float time, interpolationTime;
     private Vector3 displacement;
+    private Transform playerTransform;
     [SerializeField] private LayerMask forwardBoxes;
     [SerializeField] private LayerMask selfBoxes;
     [SerializeField] private LayerMask boundaryBoxes;
     [SerializeField] private CircleCollider2D detector;
+    [SerializeField] private float minDistance;
 
     protected override void InitNPCMoveable()
     {   // 오브젝트 풀에서 가져올 때 마다 실행해야 함. 그런데, Init 후에 실행되어야 함.
         InitMovement();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;   
     }
 
     void OnBecameVisible()
@@ -26,6 +29,7 @@ public class NPC0Moveable : NPC0
     public void InitMovement()
     {
         isMoving = false;
+        isChasing = false;
         time = maxTime;
         InitInterpolation();
     }
@@ -57,8 +61,20 @@ public class NPC0Moveable : NPC0
                 time -= Time.deltaTime;
                 if (time <= 0)
                 {
-                    // 360도 전방향 랜덤 벡터
-                    displacement = stepSize * Random.insideUnitCircle.normalized;
+                    if (isChasing)
+                    {
+                        displacement = stepSize * (playerTransform.position - transform.position).normalized;
+                        if (Vector3.Distance(playerTransform.position, transform.position) < minDistance)
+                        {
+                            displacement = Vector3.zero;
+                        }
+                    }
+                    else
+                    {
+                        // 360도 전방향 랜덤 벡터
+                        displacement = stepSize * Random.insideUnitCircle.normalized;
+
+                    }
 
                     // NPC?Moveable은 어떤 충돌박스 내에 이미 있으면 밖으로 탈출하도록 움직일 수 있어야 하므로 Forward충돌박스와는 충돌하지 않고,
                     // NPC?Moveable이 어떠한 충돌박스에도 겹치지 않으면 Forward충돌박스와 충돌하여야 한다.
@@ -88,6 +104,22 @@ public class NPC0Moveable : NPC0
                     time = maxTime;
                 }
             }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform == playerTransform)
+        {
+            isChasing = true;
+        } 
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform == playerTransform)
+        {
+            isChasing = false;
         }
     }
     void OnDrawGizmos()
