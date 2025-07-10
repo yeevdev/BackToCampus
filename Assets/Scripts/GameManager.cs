da8f98f28168a27d6cd2af7dcd0fe3a40d3bd2c2
Kunwoo Kim <starryeev@gmail.com>
2025-07-10 10:23:43 +0900

    fix: 임시에셋 수정, 게임 씬에서 플레이 전환하도록 수정
    - 게임시작버튼, 게임 타이틀 등 임시에셋 변경 및 적용
    - StartScene, GameScene 분리되어있던 구조에서 GameScene에서 시작하도록 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,35 @@
 // GameManager.cs
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject startUI;
+    [SerializeField] private GameObject joystick;
     // 어떤 스크립트에서든 접근 가능한 '현재 월드 스크롤 속도' 변수
     // 0이면 멈춘 상태, 0보다 크면 스크롤 중인 상태입니다.
     public static float currentScrollSpeed = 0f;
+
+    void Start()
+    {
+        ReadyToStart();
+    }
+
+    void ReadyToStart()
+    {
+        Time.timeScale = 0f;
+
+        startUI.SetActive(true);
+    }
+
+    public void GameStart()
+    {
+        startUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        joystick.SetActive(true);
+    }
 }
